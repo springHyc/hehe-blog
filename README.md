c275314c027270947d52b9f7278307d43f1ce624
贺贺 <yingchun.he@100credit.com>
2019-07-19 15:05:32 +0800

    优化layout布局

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
+<!-- prettier-ignore-start -->
 # hehe-blog
 
 ## 更新说明
 
--   引入 eslint 和 prettier 做代码规范
--   引入 redux 做统一的数据处理
--   添加 antd
--   添加 antd 的按需引用
+### 20190719
+
+- 删除：`"proxy": "http://127.0.0.1:4321",`,采用nginx进行反向代理来解决前后端跨域的问题
+- 优化layout布局
+
+### 201907
+
+- 引入 eslint 和 prettier 做代码规范
+- 引入 redux 做统一的数据处理
+- 添加 antd
+- 添加 antd 的按需引用
 
 ## 项目概况
 
--   启动命令-`npm start`
--   测试 - `npm test`
--   `npm run build`
--   依赖下载：`yarn install`
+- 启动命令-`npm start`
+- 测试 - `npm test`
+- `npm run build`
+- 依赖下载：`yarn install`
+
+<!-- prettier-ignore-end -->
--- src/home/index.js
+++ src/home/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import { Layout, Menu, Breadcrumb } from 'antd';
 import './index.less';
 import PandaIcon from './pandaIcon';
 import menuConfigs from '../common/menuConfigs';
@@ -14,9 +14,7 @@ export default class Home extends Component {
     }
 
     onChangemenu = ({ item, key, keyPath, domEvent }) => {
-        debugger;
         this.setState({ selectedMenu: item.props });
-        // this.props.history.push(key);
     };
     render() {
         return (
@@ -43,13 +41,15 @@ export default class Home extends Component {
                         ))}
                     </Menu>
                 </Header>
-                <Content style={{ padding: '0 50px', margin: '128px 0 0 0' }}>
+                <Content style={{ padding: '0 50px', margin: '128px 0 0px 0' }}>
                     <Breadcrumb style={{ margin: '16px 0' }}>
                         <Breadcrumb.Item>Home</Breadcrumb.Item>
                         <Breadcrumb.Item>List</Breadcrumb.Item>
                         <Breadcrumb.Item>App</Breadcrumb.Item>
                     </Breadcrumb>
-                    <div style={{ background: '#fff', padding: 24, minHeight: '100%' }}>{this.state.selectedMenu.component}</div>
+                    <div style={{ background: '#fff', padding: 24, minHeight: 'calc(100vh - 251px)' }}>
+                        {this.state.selectedMenu.component}
+                    </div>
                 </Content>
                 <Footer>贺贺 版权所有 | 采用默认主题 | 基于 React+Antd+Node 构建©2019 | 托管于xxx</Footer>
             </Layout>
